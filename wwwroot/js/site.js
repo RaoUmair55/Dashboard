50ae875f48bf90343663d6dd5f45505f991634f0
RaoUmair55 <raoumair554@gmail.com>
2024-12-29 20:23:45 +0500

    added charts

---
+++ wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿function renderPieChart(canvasId, data) {
+    const ctx = document.getElementById(canvasId).getContext('2d');
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['High Priority', 'Medium Priority', 'Low Priority'],
+            datasets: [{
+                data: data,
+                backgroundColor: ['#dc3545', '#ffc107', '#198754'],
+                borderColor: ['#fff', '#fff', '#fff'],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }
+    });
+}
